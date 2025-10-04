bd67c5e76bf7edeb1495e833848c2de0c509964a
mogiyoon <146794720+mogiyoon@users.noreply.github.com>
2025-10-04 21:34:58 +0900

    feat: update seoul meari

--- src/components/ProjectDetailComponent.tsx
+++ src/components/ProjectDetailComponent.tsx
@@ -61,13 +61,7 @@ const ProjectDetailComponent: React.FC<ProjectDetailComponentProps> = ({ project
                 <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4 justify-items-center">{project.screenshots.map((screenshot, index) => (<div key={index} className="text-center"><p className="font-semibold mb-2">{screenshot.title}</p><img src={screenshot.src} alt={screenshot.title} className="w-full max-w-[250px] rounded-lg shadow-md border" onError={handleImageError} /></div>))}</div>
             </section>
             <hr className="my-12 border-t-2 border-gray-200" />
-
-            {/* <section className="mb-16">
-                <h2 className="text-3xl sm:text-4xl font-bold text-indigo-700 mb-8 text-center">🛠️ 기술 스택</h2>
-                <ul className="list-disc list-inside text-lg text-gray-700 space-y-2 max-w-md mx-auto">{project.techStack.map((stack, index) => (<li key={index}><span className="font-semibold">{stack.category} , </span>: {stack.items.join(', ')}</li>))}</ul>
-            </section>
-            <hr className="my-12 border-t-2 border-gray-200" /> */}
-
+            
             {project.developmentProcess && (<section className="mb-16"><h2 className="text-3xl sm:text-4xl font-bold text-indigo-700 mb-8 text-center">🚀 개발 과정 상세</h2><div className="space-y-10">{project.developmentProcess.map((step, index) => (<div key={index}><h3 className="text-2xl font-semibold text-gray-800 mb-4 border-l-4 border-indigo-500 pl-4">{step.title}</h3><p className="text-lg text-gray-700 leading-relaxed whitespace-pre-wrap ml-2">{step.content}</p></div>))}</div></section>)}
             <hr className="my-12 border-t-2 border-gray-200" />
 
